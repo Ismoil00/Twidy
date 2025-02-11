70ca31f8fd40125ed65b319db4d1a343bf99997e
tom & jerry <testov@gmail.com>
2025-02-11 17:25:45 +0500

    working on custom modal creation

---
+++ frontend/src/components/modal.tsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+
+interface ModalProps {
+  children: React.ReactNode;
+}
+
+export default function Modal({ children }: ModalProps) {
+  const [open, setOpen] = useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <>
+      <Button variant="outlined" onClick={handleClickOpen}>
+        Open alert dialog
+      </Button>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">"title"</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Content
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Disagree</Button>
+          <Button onClick={handleClose}>Agree</Button>
+        </DialogActions>
+      </Dialog>
+    </>
+  );
+}
